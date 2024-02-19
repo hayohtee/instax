3231ba32f8919404acef79cae4359030080131a9
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-02-19 19:33:10 +0100

    Update my_user.dart

--- packages/user_repository/lib/src/models/my_user.dart
+++ packages/user_repository/lib/src/models/my_user.dart
@@ -1,1 +1,45 @@
-class MyUser {}
+import 'package:equatable/equatable.dart';
+
+class MyUser extends Equatable {
+  const MyUser({
+    required this.id,
+    required this.email,
+    required this.name,
+    required this.picture,
+  });
+
+  final String id;
+  final String email;
+  final String name;
+  final String? picture;
+
+  bool get isEmpty => this == MyUser.empty;
+  bool get isNotEmpty => this != MyUser.empty;
+
+  static const empty = MyUser(
+    id: '',
+    email: '',
+    name: '',
+    picture: '',
+  );
+
+  @override
+  List<Object?> get props => [id, email, name, picture];
+
+  MyUser copyWith({
+    String? id,
+    String? email,
+    String? name,
+    String? picture,
+  }) {
+    return MyUser(
+      id: id ?? this.id,
+      email: email ?? this.email,
+      name: name ?? this.name,
+      picture: picture ?? this.picture,
+    );
+  }
+
+  @override
+  bool? get stringify => true;
+}
