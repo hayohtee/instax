cbb3decae8a72afb4d25c0296a70e49f5f496828
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-02-20 15:55:10 +0100

    Add String parameter to reset password method

--- packages/user_repository/lib/src/user_repo.dart
+++ packages/user_repository/lib/src/user_repo.dart
@@ -4,5 +4,5 @@ abstract class UserRepository {
   Future<void> signIn(String email, String password);
   Future<MyUser> signUp(MyUser user, String password);
   Future<void> logOut();
-  Future<void> resetPassword();
+  Future<void> resetPassword(String email);
 }
