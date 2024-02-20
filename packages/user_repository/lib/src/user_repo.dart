ceeaf67c5a7c84e55bc09c0686631449d40c642d
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-02-20 14:41:35 +0100

    Update UserRepository

--- packages/user_repository/lib/src/user_repo.dart
+++ packages/user_repository/lib/src/user_repo.dart
@@ -1,1 +1,8 @@
-abstract class UserRepository {}
+import '../user_repository.dart';
+
+abstract class UserRepository {
+  Future<void> signIn(String email, String password);
+  Future<MyUser> signUp(MyUser user, String password);
+  Future<void> logOut();
+  Future<void> resetPassword();
+}
