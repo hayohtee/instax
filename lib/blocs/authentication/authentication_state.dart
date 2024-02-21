b7191880d6d6e487ecf9b875aac37086f2c129d5
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-02-21 16:39:44 +0100

    Update AuthenticationState

--- lib/blocs/authentication/authentication_state.dart
+++ lib/blocs/authentication/authentication_state.dart
@@ -1,6 +1,33 @@
 part of 'authentication_bloc.dart';
 
-@immutable
-sealed class AuthenticationState {}
+enum AuthenticationStatus { authenticated, unauthenticated, unknown }
 
-final class AuthenticationInitial extends AuthenticationState {}
+final class AuthenticationState extends Equatable {
+  const AuthenticationState._({
+    this.status = AuthenticationStatus.unknown,
+    this.user,
+  });
+
+  factory AuthenticationState.unknown() {
+    return const AuthenticationState._();
+  }
+
+  factory AuthenticationState.authenticated(User user) {
+    return AuthenticationState._(
+      user: user,
+      status: AuthenticationStatus.authenticated,
+    );
+  }
+
+  factory AuthenticationState.unauthenticated() {
+    return const AuthenticationState._(
+      status: AuthenticationStatus.unauthenticated,
+    );
+  }
+
+  final User? user;
+  final AuthenticationStatus status;
+
+  @override
+  List<Object?> get props => [user, status];
+}
