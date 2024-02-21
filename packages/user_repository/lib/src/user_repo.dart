536aa15f6c6c76e23f882fe63785788ec127df24
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-02-21 15:57:17 +0100

    Add user getter method to UserRepository

--- packages/user_repository/lib/src/user_repo.dart
+++ packages/user_repository/lib/src/user_repo.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 import '../user_repository.dart';
 
 abstract class UserRepository {
+  Stream<User?> get user;
   Future<void> signIn(String email, String password);
   Future<MyUser> signUp(MyUser user, String password);
   Future<void> logOut();
