ca8bcfb573a05a9dab3f4e840d46d4c6614684e8
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-02-21 16:40:02 +0100

    Update AuthenticationEvent

--- lib/blocs/authentication/authentication_event.dart
+++ lib/blocs/authentication/authentication_event.dart
@@ -1,4 +1,22 @@
 part of 'authentication_bloc.dart';
 
 @immutable
-sealed class AuthenticationEvent {}
+sealed class AuthenticationEvent extends Equatable {
+  const AuthenticationEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+final class AuthenticationUserChanged extends AuthenticationEvent {
+  const AuthenticationUserChanged(this.user);
+
+  final User? user;
+
+  @override
+  List<Object?> get props => [user];
+}
+
+final class AuthenticationLogoutRequested extends AuthenticationEvent {
+  const AuthenticationLogoutRequested();
+}
