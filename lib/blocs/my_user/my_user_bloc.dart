4912acf2f0ef5034035ad4ab375bed29529501e5
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-02-22 11:34:20 +0100

    Create my_user bloc files

--- lib/blocs/my_user/my_user_bloc.dart
+++ lib/blocs/my_user/my_user_bloc.dart
@@ -1,13 +1,25 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+import 'package:user_repository/user_repository.dart';
 
 part 'my_user_event.dart';
 part 'my_user_state.dart';
 
 class MyUserBloc extends Bloc<MyUserEvent, MyUserState> {
-  MyUserBloc() : super(MyUserInitial()) {
-    on<MyUserEvent>((event, emit) {
-      // TODO: implement event handler
+  final UserRepository _userRepository;
+
+  MyUserBloc(UserRepository userRepository)
+      : _userRepository = userRepository,
+        super(const MyUserState.loading()) {
+    on<GetMyUser>((event, emit) async {
+      try {
+        final user = await _userRepository.getMyUser(event.myUserId);
+        emit(MyUserState.success(user));
+      } catch (e) {
+        debugPrint(e.toString());
+        emit(const MyUserState.failure());
+      }
     });
   }
 }
--- lib/blocs/my_user/my_user_state.dart
+++ lib/blocs/my_user/my_user_state.dart
@@ -1,6 +1,21 @@
 part of 'my_user_bloc.dart';
 
-@immutable
-sealed class MyUserState {}
+enum MyUserStatus { success, loading, failure }
 
-final class MyUserInitial extends MyUserState {}
+class MyUserState extends Equatable {
+  const MyUserState._({
+    this.status = MyUserStatus.loading,
+    this.user,
+  });
+
+  const MyUserState.loading() : this._();
+  const MyUserState.success(MyUser user)
+      : this._(user: user, status: MyUserStatus.success);
+  const MyUserState.failure() : this._(status: MyUserStatus.failure);
+
+  final MyUserStatus status;
+  final MyUser? user;
+
+  @override
+  List<Object?> get props => [status, user];
+}
