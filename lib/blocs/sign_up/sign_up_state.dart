e72d355059ed8dd6735439f5d521fbe65a8b2cda
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-02-22 12:09:54 +0100

    Create sign_up bloc files

--- lib/blocs/sign_up/sign_up_state.dart
+++ lib/blocs/sign_up/sign_up_state.dart
@@ -1,6 +1,24 @@
 part of 'sign_up_bloc.dart';
 
-@immutable
-sealed class SignUpState {}
+abstract class SignUpState extends Equatable {
+  const SignUpState();
 
-final class SignUpInitial extends SignUpState {}
+  @override
+  List<Object?> get props => [];
+}
+
+final class SignUpInitial extends SignUpState {
+  const SignUpInitial();
+}
+
+final class SignUpSuccess extends SignUpState {
+  const SignUpSuccess();
+}
+
+final class SignUpFailure extends SignUpState {
+  const SignUpFailure();
+}
+
+final class SignUpProcess extends SignUpState {
+  const SignUpProcess();
+}
