37c27be83f800e83aad5c8b92da11bfb5ce3f618
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-02-22 12:33:50 +0100

    Update MainApp

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:instax/blocs/authentication/authentication_bloc.dart';
+import 'package:instax/blocs/sign_in/sign_in_bloc.dart';
+import 'package:instax/blocs/sign_up/sign_up_bloc.dart';
+import 'package:user_repository/user_repository.dart';
+
+import 'app_view.dart';
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({required this.userRepository, super.key});
+
+  final UserRepository userRepository;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => AuthenticationBloc(userRepository)),
+        BlocProvider(create: (_) => SignInBloc(userRepository)),
+        BlocProvider(create: (_) => SignUpBloc(userRepository)),
+      ],
+      child: const MyAppView(),
     );
   }
 }
